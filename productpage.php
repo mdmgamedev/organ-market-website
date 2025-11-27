9692bd42d81cb9a96570b45ff524eb1c3dd99c37
mdmtodebil1 <maciejsiekiera000@outlook.com>
2025-11-27 17:53:53 +0100

    Updated productpage

--- productpage.php
+++ productpage.php
@@ -1,10 +1,28 @@
 <?php
 $requested_product_id = htmlspecialchars($_GET["product_id"]);
 
+$product_name = "default";
+$product_desc = "default";
+$product_price = 0.00;
+$product_stock = 0;
+
 include("include/db.php");
 
+if(!is_numeric($requested_product_id)) {
+    die("Invalid ID");
+}
 $sql = "SELECT product_id,product_name,product_desc,product_price FROM products WHERE product_id = " . $requested_product_id . ";";
 $result = $conn->query($sql);
+
+
+if ($result && $result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $product_name = $row['product_name'];
+    $product_desc = $row['product_desc'];
+    $product_price = $row['product_price'];
+} else {
+    echo "Product not found.";
+}
 
 ?>
 
@@ -13,7 +31,7 @@ $result = $conn->query($sql);
 
     <head>
         <meta charset="UTF-8">
-        <title>product_name - Organ Market</title>
+        <title><?php echo $product_name;?> - Organ Market</title>
         <?php require ("include/head.php");?>
 
     </head>
@@ -21,11 +39,27 @@ $result = $conn->query($sql);
         <?php include ("include/navbar.php");?>
         <div class="productpage-grid">
             <div class="productpage-image-pane">
-                <img src="res/products/product_1.png">
+                <img src="res/products/<?php echo "product_" . $requested_product_id . ".png" ?>">
             </div>
             <div class="productpage-info-pane">
-                <h1>product_name</h1>
-                <h2></h2>
+                <h1><?php echo $product_name;?></h1>
+                <hr>
+                <h2><?php echo $product_stock?> currently in stock</h2>
+                <hr>
+                <div class="button-container">
+                    <button class="buy-button">Buy</button>
+                    <button class="add-to-cart-button">Add to cart</button>
+                </div>
+            </div>
+            <div class="productpage-desc-pane">
+                <div class="productpage-desc-container">
+                    <div class="description">
+                        product description
+                    </div>
+                    <div class="payment-and-shipping">
+                        payment options
+                    </div>
+                </div>
             </div>
         </div>
     </body>
