09f57bb16224eb497a0adf76327552c79dfb7584
mdmtodebil1 <maciejsiekiera000@outlook.com>
2025-12-07 16:17:02 +0100

    Minor changes

--- search.php
+++ search.php
@@ -1,7 +1,14 @@
 <?php
-
 include("include/db.php");
 
+$search_query = isset($_GET["search"]) ? $_GET["search"] : '';
+
+$sql = "SELECT * FROM products WHERE product_name LIKE ?";
+$stmt = $conn->prepare($sql);
+$like_search = "%" . $search_query . "%";
+$stmt->bind_param("s", $like_search);
+$stmt->execute();
+$result = $stmt->get_result();
 ?>
 
 <!DOCTYPE html>
@@ -16,8 +23,24 @@ include("include/db.php");
         <div class="grid-container">
             <div class="grid-item-full-small">
                 <h1>Search</h1>
-                <input type="text">
+                <form class="searchbar" action="search.php" method="GET">
+                    <input name="search" type="text">
+                    <input type="submit">
+                </form>
             </div>
+            <div class="grid-item-full-big">
+            <?php
+                if ($result->num_rows > 0) {
+                    echo "<ul class=\"search-results-list\">";
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<li>" . htmlspecialchars($row['product_name']) . " - $" . htmlspecialchars($row['product_price']) . "</li>";
+                    }
+                    echo "</ul>";
+                } else {
+                    echo "<p>No results found.</p>";
+                }
+                ?>
+        </div>
         </div>
     </body>
 </html>
